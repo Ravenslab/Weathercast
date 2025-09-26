3d4400904a8a52243e0db51b583b54212e2c8384
Mohammad Hossein Sadeghi <mohammad79std@gmail.com>
2025-09-26 22:49:18 +0330

    add: prettier and apply to existed files.

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Map from "@/components/MapWrapper";
+import Map from '@/components/MapWrapper';
 
 function Page() {
-    return (
-        <div className="max-w-5xl mx-auto py-8">
-            <h1 className="text-2xl font-bold mb-4">My Map</h1>
-            <Map />
-        </div>
-    );
+  return (
+    <div className="max-w-5xl mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">My Map</h1>
+      <Map />
+    </div>
+  );
 }
 
 export default Page;
